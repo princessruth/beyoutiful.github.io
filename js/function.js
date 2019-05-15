3ab302404b4e4ad6eae165989df554d074502c2a
princessruth <42935365+princessruth@users.noreply.github.com>
2019-05-15 08:32:39 +0700

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,22 @@
+// START EFFECT 1 PRODUCT
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+// END EFFECT 1 PRODUCT
+
+// START EFFECT 2 PRODUCT
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+  snippet.forEach(function (snippet) {
+    snippet.addEventListener('mouseout', function (event) {
+      if (event.target.parentNode.tagName === 'figure') {
+        event.target.parentNode.classList.remove('hover')
+      } else {
+        event.target.parentNode.classList.remove('hover')
+      }
+    });
+  });
+}
+// END EFFECT 2 PRODUCT
